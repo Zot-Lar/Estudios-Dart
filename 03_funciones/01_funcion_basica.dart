64c45f0585441b9c1b0002025b72cbf2eb027692
Zot-Lar <zotilas.arnikov@gmail.com>
2021-09-23 14:59:46 -0500

    Funciones basicas

---
+++ 03_funciones/01_funcion_basica.dart
@@ -0,0 +1,14 @@
+
+
+
+void main() {//funcion (dentro d elos parentesis deben ir argumentos en caso de haberlos)
+ 
+ var mensaje = saludar(); //cree una variable llamada mensaje que llama a mi funcion de saludar
+ print(mensaje); //imprime la variable mensaje
+
+  
+}
+
+String saludar() { //creo la funcion saludar que sera llamada en una variable y especificp que tipo de dato contiene, en este caos Sting por que son letras mi saludo
+  return "Hola imbeciles!"; //retorno algo, en este caso un saludo (contenido de mi función)
+}
