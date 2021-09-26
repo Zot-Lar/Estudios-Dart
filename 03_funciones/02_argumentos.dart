0a5aed49f4e73747042bded407dedf91a429b310
Zot-Lar <zotilas.arnikov@gmail.com>
2021-09-26 10:28:59 -0500

    Argumentos de una funcion (POSICIONALES)+ Update (Argumentos por NOMBRE)

--- 03_funciones/02_argumentos.dart
+++ 03_funciones/02_argumentos.dart
@@ -3,11 +3,16 @@ void saludar( String mensaje, [String nombre = "<Incerte nombre>", int edad = 90
  print("$mensaje $nombre - $edad");
 }
 
+//para la funcion saludarII el mensaje, nombre y la edad seran argumentos "por nombre"
+void saludarII(String mensaje, { required String? nombre, int? veces} ) { //entre llaves se indica que son argumentos por nombre* y mensaje al estar fuera de las llaves indico que es posicional y por ende siempre ira primero pues esta literalmente primero (Argumento posicional)
+print("SaludarII: $mensaje $nombre - $veces");                            //mensaje al ser posicional es obligatorio (si estuviera entre corchetes seria posicional opcional) y de los Argumentos por nombre el de "nombre" es obligatorio y por eso tiene la palabra "required".
+}
+
 main( ) {
 
  saludar("Joder! que se me olvida!", "Pajonor", 73);//aca llamo a mi funcion saludar definida arriba, en el parentesis me pide meter o cumplic con lo definido en la funcion saludar.
 //Al ser argumentos posicionales deben ir en el orden que tienen en donde se define la funcion saludar.
-  
+ saludarII( "Nota se suicidio", nombre: 'Toby', veces: 20);//Si quito argumento "Veces" no pasa nad apor ser opcional, aun asi si decido usarlo igualmente es un argumento por nombre.
 }
 /**Funcion que defino es [saludar] y el [mensaje, el nombre y la edad] Son sus argumentos de esa funcion. Los que estan dentro de los corchetes son
  * argumentos que defini ncomo OPCIONALES, Mientras que el argumento [mensaje] al no estar entre corchetes es un argumento OBLIGATORIO.
